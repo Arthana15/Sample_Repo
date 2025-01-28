8c3f4f7a671fea247a0842f032cb17d658b3e44b
ArthanaManoj15 <arthanamanoj97@gmail.com>
2025-01-28 10:44:34 +0530

    AccessModifier

---
+++ samplemaven/src/main/java/inheritance/AccessModifier3.java
@@ -0,0 +1,13 @@
+package inheritance;
+
+import accessmodifiers.AccessModifier1;
+
+public class AccessModifier3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AccessModifier1 obj=new AccessModifier1();
+		obj.display();
+	}
+
+}
